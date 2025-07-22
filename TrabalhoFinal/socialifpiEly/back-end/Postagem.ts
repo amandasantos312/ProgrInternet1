968d18c731ae7a635b9d91c8e28c3a88fd34f97d
amandasantos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2025-07-22 09:47:26 -0300

    modelo de postagem

--- TrabalhoFinal/socialifpiEly/back-end/Postagem.ts
+++ TrabalhoFinal/socialifpiEly/back-end/Postagem.ts
@@ -1,16 +1,20 @@
+import { Comentario } from './types';
+
 export class Postagem {
     private id: number;
     private titulo: string;
     private conteudo: string;
     private data: Date;
     private curtidas: number;
+    public comentarios: Comentario[];
 
-    constructor(id: number, titulo: string, conteudo: string, data: Date, curtidas: number) {
+    constructor(id: number, titulo: string, conteudo: string, data: Date, curtidas: number,) {
         this.id = id;
         this.titulo = titulo;
         this.conteudo = conteudo;
         this.data = data;
         this.curtidas = curtidas;
+        this.comentarios = [];
     }
 
     public getId(): number {
@@ -31,5 +35,9 @@ export class Postagem {
 
     public getCurtidas(): number {
         return this.curtidas;
+    }
+
+    public getComentarios(): Comentario[] {
+        return this.comentarios;
     }
 }
