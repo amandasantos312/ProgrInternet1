e5bf604706b6d132cf0e30290a9f7b08c9ff1983
amandasantos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2025-07-24 16:57:15 -0300

    update de terça

--- TrabalhoFinal/socialifpiEly/back-end/types.ts
+++ TrabalhoFinal/socialifpiEly/back-end/types.ts
@@ -4,12 +4,3 @@ export interface Comentario {
     conteudo: string;
     data: string;
 }
-
-export interface PostagemComComentarios {
-    id: number;
-    titulo: string;
-    conteudo: string;
-    data: Date;
-    curtidas: number;
-    comentarios: Comentario[];
-}
